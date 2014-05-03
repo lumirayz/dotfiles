5c5f302bebd23bd78d4e817f8c13b11c2b720597
lumirayz <lumirayz@gmail.com>
2014-05-03 19:25:00 +0200

    vimrc edits, vim color scheme, install.sh updates

--- install.sh
+++ install.sh
@@ -3,30 +3,43 @@
 # ======
 #  Meta
 # ======
-export DOTFILES_DIR="$HOME/.dotfiles"
+DOTFILES_DIR=`pwd`
+HOME_DIR="$HOME"
+echo "Dotfiles directory is \"$DOTFILES_DIR\"."
+echo "Home directory is \"$HOME_DIR\"."
 
 # ======
 #  Tmux
 # ======
-ln -s "$DOTFILES_DIR/tmux/tmux.bar" "~/.tmux.bar"
-ln -s "$DOTFILES_DIR/tmux/tmux.conf" "~/.tmux.conf"
+echo "Installing tmux configs…"
+ln -s "$DOTFILES_DIR/tmux/tmux.bar" "$HOME_DIR/.tmux.bar"
+ln -s "$DOTFILES_DIR/tmux/tmux.conf" "$HOME_DIR/.tmux.conf"
 
 # =====
 #  Zsh
 # =====
-ln -s "$DOTFILES_DIR/zsh/zshrc" "~/.zshrc"
+echo "Installing zsh configs…"
+ln -s "$DOTFILES_DIR/zsh/zshrc" "$HOME_DIR/.zshrc"
 
 # =====
 #  Vim
 # =====
-ln -s "$DOTFILES_DIR/vim/vimrc" "~/.vimrc"
-ln -s "$DOTFILES_DIR/vim/vimrc" "~/.gvimrc"
+echo "Installing vim configs…"
+ln -s "$DOTFILES_DIR/vim/vimrc" "$HOME_DIR/.vimrc"
+ln -s "$DOTFILES_DIR/vim/vimrc" "$HOME_DIR/.gvimrc"
 
 # =============
 #  Vim Plugins
 # =============
-if [ ! -d "~/.vim/" ]; then
-	mkdir "~/.vim"
-fi
-ln -s "$DOTFILES_DIR/vim/plugins" "~/.vim/bundle"
-ln -s "$DOTFILES_DIR/vim/pathogen/autoload" "~/vim/autoload"
+echo "Installing pathogen and vim plugin dirs…"
+VIM_DIR="$HOME_DIR/.vim"
+mkdir "$VIM_DIR"
+mkdir "$VIM_DIR/colors"
+ln -s "$DOTFILES_DIR/vim/plugins" "$VIM_DIR/bundle"
+ln -s "$DOTFILES_DIR/vim/pathogen/autoload" "$VIM_DIR/autoload"
+
+# ============
+#  Vim Colors
+# ============
+echo "Installing vim colors…"
+ln -s "$DOTFILES_DIR/vim/colors/molokai/colors/molokai.vim" "$VIM_DIR/colors/molokai.vim"
